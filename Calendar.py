c01739166a087833ed1a9d530526d8aa4abcef15
islammaishaa <141263655+islammaishaa@users.noreply.github.com>
2025-10-01 23:38:18 -0400

    Update Calendar.py

--- Calendar.py
+++ Calendar.py
@@ -6,16 +6,36 @@ from datetime import datetime
 url = "https://www.ccny.cuny.edu/registrar/fall"
 response = requests.get(url)
 
-soup = BeautifulSoup(response.content, "html.parser")
-#print(soup.title.text)
+content = BeautifulSoup(response.content, "html.parser")
+#print(content.title.text)
 
 rows = []
-for row in soup.find_all("tr"):
+for row in content.find_all("tr"):
     cells = [c.get_text(" ", strip=True) for c in row.find_all("td")]
-    if len(cells) == 3:   
+    if len(cells) == 3:   # only rows with 3 columns
         rows.append(cells)
-        
+     
 df = pd.DataFrame(rows, columns=["date_str", "dow", "text"])
 dates = []
 dows = []
 texts = []
+
+for _, r in df.iterrows():
+    date_str = r["date_str"]
+    description = r["text"]
+    try:
+        d = datetime.strptime(date_str.strip() + " 2021", "%B %d %Y")
+        dates.append(d)
+        dows.append(d.strftime("%A"))
+        texts.append(description)
+    except:
+        continue
+
+calendar_df = pd.DataFrame({
+    "dow": dows,
+    "text": texts
+}, index=pd.to_datetime(dates))
+
+# Step 8. Sort by date and preview
+calendar_df = calendar_df.sort_index()
+calendar_df.head(10)
