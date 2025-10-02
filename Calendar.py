96a5b8d84182b2fc89f72a79ce85a22cd2e71d77
islammaishaa <141263655+islammaishaa@users.noreply.github.com>
2025-10-01 23:15:14 -0400

    Update Calendar.py

--- Calendar.py
+++ Calendar.py
@@ -2,3 +2,9 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 from datetime import datetime
+
+url = "https://www.ccny.cuny.edu/registrar/fall"
+response = requests.get(url)
+
+soup = BeautifulSoup(response.content, "html.parser")
+#print(soup.title.text)
